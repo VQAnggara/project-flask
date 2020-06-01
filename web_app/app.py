10a7560b5c5f4de71d313dcb4ebf448ed0c1cf6b
VQAnggara <anggaravic@gmail.com>
2020-06-01 03:14:51 +0300

    [project-02] Koneksi ke database Postgresql

--- web_app/app.py
+++ web_app/app.py
@@ -12,4 +12,18 @@ def create_app():
     def about():
         return render_template('about.html', TITLE = 'Project Flask')
 
+    @app.route('/testdb')
+    def testdb():
+        import psycopg2
+
+        con = psycopg2.connect('dbname=project-flask user=devuser password=devpassword host=postgres')
+        cur = con.cursor()
+
+        cur.execute('select * from page;')
+
+        id, title = cur.fetchone()
+        con.close()
+
+        return 'Jika ini keluar maka test database berhasil: {} - {}'.format(id, title)
+
     return app
